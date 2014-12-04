20c54b632faf5b07c11a342e245724d5b22ac0dc
Шаталов Виктор Михайлович <viktor.shatalov@privatbank.ua>
2014-12-04 16:54:27 +0200

    добавил CRUD действия, для моделей связаных с сцециальностями

---
+++ backend/views/directions/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ListView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Directions';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="directions-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Directions', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model->title), ['view', 'id' => $model->id]);
+        },
+    ]) ?>
+
+</div>
---
+++ backend/views/institutes/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ListView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Institutes';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="institutes-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Institutes', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model->title), ['view', 'id' => $model->id]);
+        },
+    ]) ?>
+
+</div>
